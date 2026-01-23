c89f189f316e38a8e38606ee0848053fc6a99820
Leonardo <joelolaya681@gmail.com>
2026-01-23 10:18:48 -0500

    segundo commit

---
+++ src/app/core/core-module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing-module';
+import { Layout } from './layout/layout';
+
+
+@NgModule({
+  declarations: [
+    Layout
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class CoreModule { }
